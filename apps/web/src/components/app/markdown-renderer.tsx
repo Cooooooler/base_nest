8218b3ed46c3da91d652046de7996eb16368822d
lumenglin <15755967688@163.com>
2026-07-05 14:03:32 +0800

    refactor: simplify token generation and improve image element handling in markdown renderer

--- apps/web/src/components/app/markdown-renderer.tsx
+++ apps/web/src/components/app/markdown-renderer.tsx
@@ -170,8 +170,9 @@ const COMPONENTS: Components = {
   hr: (props) => <hr className='my-4 border-t border-border/50' {...props} />,
 
   // ====== Images ======
-  // eslint-disable-next-line @next/next/no-img-element
+
   img: ({ alt, src, ...props }) => (
+    // eslint-disable-next-line @next/next/no-img-element
     <img
       alt={alt || ''}
       src={src}
